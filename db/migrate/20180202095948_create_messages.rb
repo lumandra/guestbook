fd786be571a49dde6ae62d38873c16fa622736ab
Pecherina Marina <lumandra7@gmail.com>
2018-02-02 13:06:19 +0300

    init Rails app

---
+++ db/migrate/20180202095948_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.string :name
+      t.string :email
+      t.text :message
+      t.string :state, default: 'new'
+
+      t.timestamps
+    end
+  end
+end
